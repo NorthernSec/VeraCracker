3df229cff2f70105e08e67d3b3158cf29a4d8e0f
korridor <26689068+korridor@users.noreply.github.com>
2017-12-25 21:53:28 +0100

    Added multiple features
     - Added macOS support
     - Added optional argument to pass the VeraCrypt binary path
     - Removed blanko check before each check in linuxCrack
     - Added averange time in printResults
     - Formatted code with autopep8
     - Added -t to the VeraCrypt attributes

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # VeraCracker
 Veracrypt Password Cracker <br />
-This script will go through a list of passwords and try these against the specified volume. If succeeded, it will mount
- the partition. 
+This script will go through a list of passwords and try these against the specified volume. If succeeded, it will mount the partition. 
 
 **Note:** This project is currently only working under **Python 3.x** on **Windows** and Linux systems. <br />
 **Note:** **No dependencies** are needed, but **VeraCrypt has to be installed**
 
-##Disclaimer
-This tool is for educational purposes only and is not intended to be put into practise unless you have authorised access
- to the system you are trying to break into
- 
-##License
+## Disclaimer
+This tool is for educational purposes only and is not intended to be put into practise unless you have authorised access to the system you are trying to break into.
+
+## License
 This software is licensed under the "Original BSD License".
 ```
   (C) 2015  NorthernSec          https://github.com/NorthernSec
--- VeraCracker.py
+++ VeraCracker.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 #
 # VeraCrypt volume password cracker
-
+#
 # Copyright (c) 2015	NorthernSec
 # Copyright (c) 2015	Pieter-Jan Moreels
 # This software is licensed under the Original BSD License
@@ -17,133 +17,156 @@ import time
 from datetime import datetime
 
 # Constants
-VeraWinPath = '"c:\\Program Files\\VeraCrypt\\"'
-VeraWinCMD = 'VeraCrypt.exe /v "%s" /q /p "%s" /s /l %s'
+VeraWinPath = '"c:\\Program Files\\VeraCrypt\\VeraCrypt.exe"'
+VeraWinAttributes = ' /v "%s" /q /p "%s" /s /l %s'
 VeraWinProcList = "query process"
 VeraWinProcName = "veracrypt.exe"
-VeraLinuxCMD = 'veracrypt %s -p %s --non-interactive'
+VeraMacPath = '/Applications/VeraCrypt.app/Contents/MacOS/VeraCrypt'
+VeraLinuxPath = 'veracrypt'
+VeraLinuxAttributes = ' -t %s -p %s --non-interactive'
 
 # Functions
-def isVeraRunning():
-  return True if ''.join(os.popen(VeraWinProcList).readlines()).find(VeraWinProcName) >= 0 else False
 
-  
+
+def isVeraRunning():
+    return True if ''.join(os.popen(VeraWinProcList).readlines()).find(VeraWinProcName) >= 0 else False
+
+
 def progressbar(it, prefix="Cracking ", size=50):
-  count = len(it)
-  def _show(_i):
-    if count != 0 and sys.stdout.isatty():
-      x = int(size * _i / count)
-      sys.stdout.write("%s[%s%s] %i/%i\r" % (prefix, "#" * x, " " * (size - x), _i, count))
-      sys.stdout.flush()
-  _show(0)
-  for i, item in enumerate(it):
-    yield item
-    _show(i + 1)
-  sys.stdout.write("\n")
-  sys.stdout.flush()
+    count = len(it)
+
+    def _show(_i):
+        if count != 0 and sys.stdout.isatty():
+            x = int(size * _i / count)
+            sys.stdout.write("%s[%s%s] %i/%i\r" %
+                             (prefix, "#" * x, " " * (size - x), _i, count))
+            sys.stdout.flush()
+    _show(0)
+    for i, item in enumerate(it):
+        yield item
+        _show(i + 1)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
 
 def checkRequirements():
-  if platform.system() == "Windows":
-    print("TODO: Is veracrypt installed")
-    if isVeraRunning():
-      sys.exit("Please close VeraCrypt before running this script")
+    if platform.system() == "Windows":
+        print("TODO: Is veracrypt installed")
+        if isVeraRunning():
+            sys.exit("Please close VeraCrypt before running this script")
+        try:
+            os.chdir("%s:" % args.m)
+            sys.exit("Please specify an unused mountpoint")
+        except Exception as e:
+            pass
+
+
+def windowsCrack(p, veracryptPath):
+    os.popen(veracryptPath + VeraWinAttributes % (args.v, p, args.m))
+    while True:
+        if isVeraRunning():
+            time.sleep(0.1)
+        else:
+            break
     try:
-      os.chdir("%s:"%args.m)
-      sys.exit("Please specify an unused mountpoint")
-    except Exception as e:
-      pass
-  elif platform.system() == "Linux":
-    print("TODO: Is veracrypt installed")
+        os.chdir("%s:" % args.m)
+        return True
+    except:
+        return False
 
 
-def windowsCrack(p):
-  os.popen(VeraWinPath + VeraWinCMD%(args.v, p, args.m))
-  while True:
-    if isVeraRunning():
-      time.sleep(0.1)
+def linuxCrack(p, veracryptPath):
+    cmd = veracryptPath + VeraLinuxAttributes % (args.v, p)
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+
+    procreturn = str(
+        out, "utf-8").strip() if out else str(err, "utf-8").strip()
+    if procreturn.find("Error: Operation failed due to one or more of the following:") != -1:
+        return False
+    elif procreturn == "Error: Failed to obtain administrator privileges.":
+        sys.exit("This script requires root previleges")
+    elif procreturn.find("No such file or directory") != -1:
+        sys.exit(
+            "VeraCrypt cannot be found: Install VeraCrypt or use the -b argument")
+    elif procreturn == "Error: The volume you are trying to mount is already mounted.":
+        sys.exit("This volume is already mounted")
     else:
-      break
-  try:
-    os.chdir("%s:"%args.m)
-    return True
-  except:
-    return False
+        print(procreturn)
+        return True
 
-
-def linuxCrack(p):
-  
-  cmd=VeraLinuxCMD%(args.v, "thisisnotthecorrectpassword")
-  process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  out, err = process.communicate()
-  err_message = str(out, "utf-8").strip() if out else str(err, "utf-8").strip()
-  
-  cmd=VeraLinuxCMD%(args.v, p)
-  process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  out, err = process.communicate()
-  
-  procreturn = str(out, "utf-8").strip() if out else str(err, "utf-8").strip()
-  if procreturn == err_message:
-    return False
-  elif procreturn == "Error: Failed to obtain administrator privileges.":
-    sys.exit("This script requires root previleges")
-  elif procreturn == "Error: The volume you are trying to mount is already mounted.":
-    sys.exit("This volume is already mounted")
-  else:
-    print(procreturn)
-    return True
 
 def printResults(startTime, tried):
-  print("Cracking duration: %s"%str(datetime.now()-startTime))
-  print("Passwords tried:   %s"%tried)
+    print("Cracking duration: %s" % str(datetime.now() - startTime))
+    print("Averange time per try: %s" %
+          str((datetime.now() - startTime) / tried))
+    print("Passwords tried:   %s" % tried)
 
 
-
-# Set variables based on platform
-if platform.system() == "Windows":
-  crack=windowsCrack
-elif platform.system() == "Linux":
-  crack=linuxCrack
-else:
-  sys.exit("This script is not written for your platform")
-  
 if __name__ == '__main__':
-  # Parse arguments
-  description='''VeraCrypt volume cracker'''
+    # Parse arguments
+    description = '''VeraCrypt volume cracker'''
 
-  parser = argparse.ArgumentParser(description=description)
-  parser.add_argument('-v', metavar='volume',     type=str, required=True, help='Path to volume')
-  parser.add_argument('-p', metavar='list',       type=str, nargs="?",     help='Password list')
-  if platform.system() == "Windows":
-    parser.add_argument('-m', metavar='mountpoint', type=str, required=True, help='Mountpoint for the volume to be mounted')
-  parser.add_argument('-d', action='store_true',                           help='Debug mode')
-  parser.add_argument('-o', metavar='file',       type=str,                help='Output file for untried passwords on quit')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-v', metavar='volume', type=str,
+                        required=True, help='Path to volume')
+    parser.add_argument('-p', metavar='list', type=str,
+                        nargs="?", help='Password list')
+    if platform.system() == "Windows":
+        parser.add_argument('-m', metavar='mountpoint', type=str,
+                            required=True, help='Mountpoint for the volume to be mounted')
+    parser.add_argument('-d', action='store_true',
+                        help='Debug mode')
+    parser.add_argument('-o', metavar='file', type=str,
+                        help='Output file for untried passwords on quit')
+    parser.add_argument('-b', metavar='file', type=str,
+                        help='Path to the VeraCrypt binary')
+    args = parser.parse_args()
 
-  # Check script requirements
-  checkRequirements()
-  # Get wordlist
-  wordlist=[x.strip() for x in open(args.p, 'r')] if args.p else [line.strip() for line in fileinput.input()]
-  
-  # Time to test
-  wlCopy = list(wordlist)
-  tried=0
-  startTime=datetime.now()
-  try:
-    for p in progressbar(wordlist):
-      if args.d:
-        print("[-] Trying %s"%p)
-      if crack(p):
-        print("[+] Password found! --> %s <--"%p)
+    # Get VeraCypt binary path
+    if platform.system() == "Linux":
+        veracryptPath = VeraLinuxPath
+        crack = linuxCrack
+    elif platform.system() == "Windows":
+        veracryptPath = VeraWinPath
+        crack = windowsCrack
+    elif platform.system() == "Darwin":
+        veracryptPath = VeraMacPath
+        crack = linuxCrack
+    else:
+        sys.exit("This script is not written for your platform")
+
+    if args.b:
+        veracryptPath = args.b
+
+    # Check script requirements
+    checkRequirements()
+
+    # Get wordlist
+    wordlist = [x.strip() for x in open(args.p, 'r')] if args.p else [
+        line.strip() for line in fileinput.input()]
+
+    # Time to test
+    wlCopy = list(wordlist)
+    tried = 0
+    startTime = datetime.now()
+    try:
+        for p in progressbar(wordlist):
+            if args.d:
+                print("[-] Trying %s" % p)
+            if crack(p, veracryptPath):
+                print("[+] Password found! --> %s <--" % p)
+                printResults(startTime, tried)
+                sys.exit(0)
+            wlCopy.pop(0)
+            tried += 1
+        print("Password not found")
         printResults(startTime, tried)
-        sys.exit(0)
-      wlCopy.pop(0)
-      tried+=1
-    print("Password not found")
-    printResults(startTime, tried)
-  except KeyboardInterrupt:
-    print("[!]Cracker interrupted")
-    if args.o:
-      if args.d:
-        print("[-] Saving leftover passwords to %s"%args.o)
-      f=open(args.o,'w')
-      f.write("\n".join(wlCopy))
+    except KeyboardInterrupt:
+        print("[!]Cracker interrupted")
+        if args.o:
+            if args.d:
+                print("[-] Saving leftover passwords to %s" % args.o)
+            f = open(args.o, 'w')
+            f.write("\n".join(wlCopy))
